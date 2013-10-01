c6638fd53357a320795f0a87f41380634413c6ee
Ray Rodriguez <rarodriguez@mdsol.com>
2013-10-01 17:18:58 -0400

    Initial commit of berks cookbook skeleton.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'berkshelf', :github => 'RiotGames/berkshelf', :ref => '55678'
+gem 'test-kitchen', :github => 'opscode/test-kitchen', :ref => '3f78e', :group => :integration
+gem 'kitchen-vagrant', :group => :integration
+gem 'kitchen-ec2', :group => :integration
+gem 'foodcritic', :group => :integration
+gem 'chef', '~> 11.4.2'
+gem 'knife-ec2', '~> 0.6.4'
+gem 'stove'
---
+++ metadata.rb
@@ -0,0 +1,16 @@
+name             'mesos'
+maintainer       'Medidata Solutions'
+maintainer_email 'rarodriguez@mdsol.com'
+license          'Apache 2.0'
+description      'Installs/Configures mesos'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
+# Cookbook dependencies
+%w{ chef-zookeeper }.each do |cb|
+  depends cb
+end
