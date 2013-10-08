9dc8c80f0f907925e90c1415b92ca4c98f989563
Ray Rodriguez <rarodriguez@mdsol.com>
2013-10-07 22:36:53 -0400

    Bumping version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rarodriguez@mdsol.com'
 license          'Apache 2.0'
 description      'Installs/Configures mesos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 %w{ ubuntu }.each do |os|
   supports os
