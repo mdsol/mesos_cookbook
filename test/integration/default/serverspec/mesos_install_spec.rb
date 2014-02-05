a8fffe8b0b929dfac0ce633c2804f95fa273c538
Ray Rodriguez <rarodriguez@mdsol.com>
2014-02-04 20:20:10 -0500

    Adding rhel compatible serverspec tests

--- test/integration/default/serverspec/mesos_install_spec.rb
+++ test/integration/default/serverspec/mesos_install_spec.rb
@@ -1,20 +1,20 @@
-require 'spec_helper'
+require_relative '../../../kitchen/data/spec_helper'
 
 # verify packages
-describe package('unzip') do
-  it { should be_installed }
-end
-
-describe package('default-jre-headless') do
-  it { should be_installed }
-end
-
-describe package('libcurl3') do
-  it { should be_installed }
-end
-
-describe package('mesos') do
-  it { should be_installed }
+describe 'mesos install' do
+  it 'should install required packages' do
+    case RSpec.configuration.os
+    when 'Debian'
+      expect(package 'unzip').to be_installed
+      expect(package 'default-jre-headless').to be_installed
+      expect(package 'libcurl3').to be_installed
+      expect(package 'mesos').to be_installed
+    when 'RedHat'
+      expect(package 'unzip').to be_installed
+      expect(package 'libcurl').to be_installed
+      expect(package 'mesos').to be_installed
+    end
+  end
 end
 
 describe file('/etc/init/mesos-master.conf') do
