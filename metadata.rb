f029f4127df89bbbc20787baac7b12d8a043d982
Jeff Harvey-Smith <jeff@clearstorydata.com>
2014-09-12 12:19:29 -0700

    Bump major version.  Add backticks to zk entries in attributes.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'hwilkinson@mdsol.com'
 license 'Apache 2.0'
 description 'Installs/Configures Apache Mesos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.0'
+version '2.0.0'
 
 %w( ubuntu centos amazon scientific ).each do |os|
   supports os
