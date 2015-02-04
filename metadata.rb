d551f1a33354d3fb09826b2846fa68f4a79596a8
Ray Rodriguez <rayrod2030@gmail.com>
2015-02-04 12:11:57 -0500

    Removed scientific linux from list of officially supported OS's until we test on it.

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
 # rubocop:enable Style/SingleSpaceBeforeFirstArg
 
-%w( ubuntu centos amazon scientific ).each do |os|
+%w( ubuntu debian centos amazon scientific ).each do |os|
   supports os
 end
 
