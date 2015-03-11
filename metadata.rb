4c6ee3910387cf0cd2562a5c296a6ac6986be7f7
Ray Rodriguez <rayrod2030@gmail.com>
2015-03-11 16:16:15 -0400

    Bumping version to 3.0.0

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 # rubocop:disable Style/SingleSpaceBeforeFirstArg
 name 'mesos'
-maintainer 'Medidata Solutions'
-maintainer_email 'hwilkinson@mdsol.com'
+maintainer 'Ray Rodriguez'
+maintainer_email 'rayrod2030@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures Apache Mesos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.0'
+version '3.0.0'
 # rubocop:enable Style/SingleSpaceBeforeFirstArg
 
 %w( ubuntu debian centos amazon scientific ).each do |os|
