9d5dff040ee4f534439ed20f5e78ad166fcb23d6
Robert Veznaver <r.veznaver@criteo.com>
2015-12-16 15:49:03 +0100

    Specify cgroup hierarchy root in attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,95 +1,76 @@
-# Enable default repo
-default['mesos']['repo']    = true
+# Default Java version
+default['java']['jdk_version'] = '7'
+
+# Use Mesosphere repo
+default['mesos']['repo']       = true
 
 # Mesosphere Mesos version.
-default['mesos']['version'] = '0.25.0'
+default['mesos']['version']    = '0.25.0'
 
 # Init system to use
-default['mesos']['init']    = case node['platform']
-                              when 'debian' then 'sysvinit_debian'
-                              when 'centos', 'amazon', 'scientific'
-                                node['platform_version'].to_i >= 7 ? 'systemd' : 'upstart'
-                              else 'upstart'
-                              end
+default['mesos']['init']       = case node['platform']
+                                 when 'debian'
+                                   node['platform_version'].to_i >= 8 ? 'systemd' : 'sysvinit_debian'
+                                 when 'ubuntu'
+                                   node['platform_version'].to_f >= 15.04 ? 'systemd' : 'upstart'
+                                 when 'redhat', 'centos', 'amazon', 'scientific'
+                                   node['platform_version'].to_i >= 7 ? 'systemd' : 'upstart'
+                                 else 'upstart'
+                                 end
 
 #
 # Mesos MASTER configuration
 #
 
-# Mesos master binary location
-default['mesos']['master']['bin']                       = '/usr/sbin/mesos-master'
+# Mesos master binary location.
+default['mesos']['master']['bin']                   = '/usr/sbin/mesos-master'
 
-# Ulimit to pass to mesos-master process.
-default['mesos']['master']['env']['ULIMIT']             = '-n 16384'
+# Environmental variables set before calling the mesos master process.
+default['mesos']['master']['env']['ULIMIT']         = '-n 16384'
 
-# Send stdout & stderr to syslog
-default['mesos']['master']['syslog']                    = true
+# Send stdout and stderr to syslog.
+default['mesos']['master']['syslog']                = true
 
-# mesos-master default port
-default['mesos']['master']['flags']['port']             = 5050
-
-# Directory to write mesos-master logs.
-default['mesos']['master']['flags']['log_dir']          = '/var/log/mesos'
-
-# mesos-master default logging level.  Valid options: 'INFO', 'WARNING', 'ERROR']
-default['mesos']['master']['flags']['logging_level']    = 'INFO'
-
-# mesos-master cluster name that is visible from UI.
-default['mesos']['master']['flags']['cluster']          = 'MyMesosCluster'
-
-# Where to store the persistent information stored in the Registry.
-default['mesos']['master']['flags']['work_dir']         = '/tmp/mesos'
+# Mesos master command line flags.
+# http://mesos.apache.org/documentation/latest/configuration/
+default['mesos']['master']['flags']['port']          = 5050
+default['mesos']['master']['flags']['log_dir']       = '/var/log/mesos'
+default['mesos']['master']['flags']['logging_level'] = 'INFO'
+default['mesos']['master']['flags']['cluster']       = 'MyMesosCluster'
+default['mesos']['master']['flags']['work_dir']      = '/tmp/mesos'
 
 #
 # Mesos SLAVE configuration
 #
 
-# Mesos slave binary location
-default['mesos']['slave']['bin']                        = '/usr/sbin/mesos-slave'
+# Mesos slave binary location.
+default['mesos']['slave']['bin']                    = '/usr/sbin/mesos-slave'
 
-# Ulimit to pass to mesos-slave process.
-default['mesos']['slave']['env']['ULIMIT']              = '-n 16384'
+# Environmental variables set before calling the mesos-slave process.
+default['mesos']['slave']['env']['ULIMIT']          = '-n 16384'
 
-# Send stdout & stderr to syslog
-default['mesos']['slave']['syslog']                     = true
+# Send stdout and stderr to syslog.
+default['mesos']['slave']['syslog']                 = true
 
-# mesos-slave default port
-default['mesos']['slave']['flags']['port']              = 5051
+# Mesos slave command line flags
+# http://mesos.apache.org/documentation/latest/configuration/
+default['mesos']['slave']['flags']['port']          = 5051
+default['mesos']['slave']['flags']['log_dir']       = '/var/log/mesos'
+default['mesos']['slave']['flags']['logging_level'] = 'INFO'
+default['mesos']['slave']['flags']['work_dir']      = '/tmp/mesos'
+default['mesos']['slave']['flags']['isolation']     = 'posix/cpu,posix/mem'
+default['mesos']['slave']['flags']['master']        = 'localhost:5050'
+default['mesos']['slave']['flags']['strict']        = true
+default['mesos']['slave']['flags']['recover']       = 'reconnect'
 
-# Directory to write mesos-master logs.
-default['mesos']['slave']['flags']['log_dir']           = '/var/log/mesos'
+# Workaround for setting default cgroups hierarchy root
+if node['mesos']['init'] == 'systemd'
+  default['mesos']['slave']['flags']['cgroups_hierarchy'] = '/sys/fs/cgroup'
+else
+  default['mesos']['slave']['flags']['cgroups_hierarchy'] = '/cgroup'
+end
 
-# mesos-slave default logging level.  Valid options: 'INFO', 'WARNING', 'ERROR']
-default['mesos']['slave']['flags']['logging_level']     = 'INFO'
-
-# Where to store the persistent information stored in the Registry.
-default['mesos']['slave']['flags']['work_dir']          = '/tmp/mesos'
-
-# The path to the cgroups hierarchy root
-default['mesos']['slave']['flags']['cgroups_hierarchy'] = '/cgroup'
-
-# Isolation mechanisms to use, e.g., 'posix/cpu,posix/mem', or 'cgroups/cpu,cgroups/mem',
-# or network/port_mapping (configure with flag: --with-network-isolator to enable), or
-# 'external', or load an alternate isolator module using the --modules flag.
-default['mesos']['slave']['flags']['isolation']         = 'posix/cpu,posix/mem'
-
-# This specifies how to connect to a master or a quorum of masters. This flag
-# works with 3 different techniques.  See Mesos documentation for details:
-# http://mesos.apache.org/documentation/latest/configuration
-default['mesos']['slave']['flags']['master']            = 'localhost:5050'
-
-# If stric is true, any and all recovery errors are considered fatal. If strict
-# is false, any expected errors (e.g. slave cannot recover information about
-# an executor, because the slave died right before the executor registered)
-# during recovery are ignored and as much state as possible is recovered.
-default['mesos']['slave']['flags']['strict']            = true
-
-# Whether to recover status updates and reconnect with old executors.
-# Valid options: 'reconnect', 'cleanup'. See Mesos documentation for
-# details: http://mesos.apache.org/documentation/latest/configuration
-default['mesos']['slave']['flags']['recover']           = 'reconnect'
-
-# Use the following two options if you are using Exhibitor to manage Zookeeper
+# Use the following options if you are using Exhibitor to manage Zookeeper
 # in your environment.
 
 # Zookeeper path that Mesos will use to write to.
@@ -100,8 +81,3 @@ default['mesos']['zookeeper_exhibitor_discovery']       = false
 
 # Netflix Exhibitor ZooKeeper ensemble url.
 default['mesos']['zookeeper_exhibitor_url']             = nil
-
-case node['platform_family']
-when 'rhel'
-  default['java']['jdk_version'] = '7'
-end
